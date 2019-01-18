78fff19391832d6c203726f602424684b29c4509
lilliputten <lilliputten@yandex.ru>
2019-01-18 21:22:30 +0300

    Code merged back to single file

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 /* eslint-env browser, commonjs */
-/* global React */
+/* global React, ReactDOM */
 /* --eslint-disable no-debugger, no-console */
 
 // Slomux - реализация Flux, в которой, как следует из нвазвания, что-то сломано.
@@ -121,13 +121,119 @@ var Provider = /** @class */ (function (superComponent) {
 }(React.Component));
 /*}}}*/
 
-// Suppress 'defined but not used' warnings
-if (typeof module === 'object') {
-  module.exports = {
-    __extends: __extends,
-    __assign: __assign,
-    createStore: createStore,
-    connect: connect,
-    Provider: Provider,
+// APP
+
+// Actions
+
+var ADD_TODO = 'ADD_TODO';
+
+// Action creators
+
+/** addTodo ** {{{
+ */
+var addTodo = function (todo) {
+  return ({
+    type: ADD_TODO,
+    payload: todo
+  });
+};/*}}}*/
+
+// Reducers...
+
+/** reducer ** {{{
+ */
+var reducer = function (stateList, action) {
+  // if (stateList === void 0) {
+  if (!Array.isArray(stateList)) {
+    stateList = [];
+  }
+  switch (action.type) {
+    case ADD_TODO:
+      stateList.push(action.payload);
+      return stateList;
+    default:
+      return stateList;
+  }
+};/*}}}*/
+
+/** ToDoComponent ** {{{
+ */
+var ToDoComponent = /** @class */ (function (superComponent) {
+
+  __extends(ToDoComponent, superComponent);
+
+  function ToDoComponent() {
+    var _this = superComponent !== null && superComponent.apply(this, arguments) || this;
+    _this.state = {
+      todoText: '',
+    };
+    // Bind handlers...
+    _this._addTodo = _this.addTodo.bind(_this);
+    _this._updateText = _this.updateText.bind(_this);
+    return _this;
+  }
+
+  ToDoComponent.prototype.render = function () {
+    // NOTE: Adding binded event handlers!
+    return (React.createElement('div', null,
+      React.createElement('label', null, this.props.title || 'Без названия'),
+      React.createElement('div', null,
+        React.createElement('input', {
+          value: this.state.todoText,
+          placeholder: 'Название задачи',
+          onChange: this._updateText,
+        }),
+        React.createElement('button', { onClick: this._addTodo }, 'Добавить'),
+        React.createElement('ul', null, this.props.todos.map(function (todo, idx) {
+          // TODO: To generate unique key (useless now)?
+          return React.createElement('li', { key: idx }, todo);
+        })))
+    ));
   };
-}
+
+  ToDoComponent.prototype.updateText = function (e) {
+    var value = e.target.value;
+    // this.state.todoText = value; // NOTE: Error!
+    this.setState({ todoText: value });
+  };
+
+  ToDoComponent.prototype.addTodo = function () {
+    this.props.addTodo(this.state.todoText);
+    // this.state.todoText = ''; // NOTE: Error!
+    this.setState({ todoText: '' });
+  };
+
+  return ToDoComponent;
+
+}(React.Component));/*}}}*/
+
+/*{{{ Create app... */
+
+var ToDo = connect(
+  // mapStateToProps
+  function (state) {
+    return ({
+      todos: state,
+    });
+  },
+  // mapDispatchToProps
+  function (dispatch) {
+    return ({
+      addTodo: function (text) {
+        return dispatch(addTodo(text));
+      },
+    });
+  }
+)(ToDoComponent);
+
+// init
+ReactDOM.render(
+  React.createElement(
+    Provider, // Component
+    { store: createStore(reducer, []) }, // Props
+    React.createElement(ToDo, { title: 'Список задач' }) // Children
+  ),
+  document.getElementById('app')
+);
+
+/*}}}*/
